ad24a97cbb4ed9fa0d8c4812aa5979b0bcf6f811
olhasemenova <olha.sergeevna87@gmail.com>
2025-10-31 21:18:23 +0200

    add negative pow case

--- hw_3.js
+++ hw_3.js
@@ -3,6 +3,13 @@ function pow(x, n){
     if (x == 0){
         result = 0;
     }
+    else if (n < 0){
+        for (let i = 1; i <= Math.abs(n); i++){
+          result *= x;
+        }  
+        result = 1 / result;
+    }
+
     else {
         for (let i = 1; i <= n; i++){
           result *= x;
@@ -11,11 +18,13 @@ function pow(x, n){
     return result
 }
 
-console.log(pow(1,5)) // 1
-console.log(pow(0,1))// 0
-console.log(pow(2,0)) // 1
-console.log(pow(2,1)) // 2
-console.log(pow(2,2)) // 4
-console.log(pow(2,3)) // 8
-console.log(pow(3,3)) // 27
-console.log(pow(5,3)) // 125
+// console.log(pow(2,-2)) // 0.25
+// console.log(pow(3,-3)) // 0.037
+// console.log(pow(1,5)) // 1
+// console.log(pow(0,1))// 0
+// console.log(pow(2,0)) // 1
+// console.log(pow(2,1)) // 2
+// console.log(pow(2,2)) // 4
+// console.log(pow(2,3)) // 8
+// console.log(pow(3,3)) // 27
+// console.log(pow(5,3)) // 125
