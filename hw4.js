606e4a08db0882d7ea9e43825e16530191cf5b09
olhasemenova <olha.sergeevna87@gmail.com>
2025-11-04 21:32:17 +0200

    refactoring

--- hw4.js
+++ hw4.js
@@ -1,16 +1,16 @@
 function checkProbabilityTheory(count){
     if (Number.isInteger(count)){
-            let countEven = 0; // парні
-    let countOdd = 0; // непарні
-    for (let i = 0; i < count; i++){
-        let num = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+        let countEven = 0; // парні
+        let countOdd = 0; // непарні
+        for (let i = 0; i < count; i++){
+            let num = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
         // console.log(num);
-        (num % 2 == 0) ? countEven++ : countOdd++;
+            (num % 2 == 0) ? countEven++ : countOdd++;
         // console.log(countEven)
         // console.log(countOdd)
-    }
-    let evenToOddPercent = (countOdd != 0) ? (countEven  / countOdd) * 100 : 'Непарних чисел не сгенеровано';// якщо буде 0 непарних то в результаті отримаємо Infinity
-   return `Кількість згенерованих чисел: ${count}; Кількість парних: ${countEven}; Кількість непарних:${countOdd}; Відсоток парних до непарних:${evenToOddPercent} %`;
+        }
+        let evenToOddPercent = (countOdd != 0) ? `${(countEven  / countOdd) * 100} %`: 'Непарних чисел не сгенеровано';// якщо буде 0 непарних то в результаті отримаємо Infinity
+        return `Кількість згенерованих чисел: ${count}; Кількість парних: ${countEven}; Кількість непарних:${countOdd}; Відсоток парних до непарних:${evenToOddPercent}`;
     } else {
         return 'Змінна count повинна бути цілим числом!';
     }
