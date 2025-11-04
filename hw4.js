ed035d89ef67a1924df19854a9de4dcb38e2da2a
olhasemenova <olha.sergeevna87@gmail.com>
2025-11-04 21:26:46 +0200

    add check if count is integer

--- hw4.js
+++ hw4.js
@@ -1,5 +1,6 @@
 function checkProbabilityTheory(count){
-    let countEven = 0; // парні
+    if (Number.isInteger(count)){
+            let countEven = 0; // парні
     let countOdd = 0; // непарні
     for (let i = 0; i < count; i++){
         let num = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
@@ -9,7 +10,11 @@ function checkProbabilityTheory(count){
         // console.log(countOdd)
     }
     let evenToOddPercent = (countOdd != 0) ? (countEven  / countOdd) * 100 : 'Непарних чисел не сгенеровано';// якщо буде 0 непарних то в результаті отримаємо Infinity
-   return `Кількість згенерованих чисел: ${count}; Кількість парних: ${countEven}; Кількість непарних:${countOdd}; Відсоток парних до непарних:${evenToOddPercent} %` 
+   return `Кількість згенерованих чисел: ${count}; Кількість парних: ${countEven}; Кількість непарних:${countOdd}; Відсоток парних до непарних:${evenToOddPercent} %`;
+    } else {
+        return 'Змінна count повинна бути цілим числом!';
+    }
 }
 
 console.log(checkProbabilityTheory(4))
+console.log(checkProbabilityTheory('string'))
